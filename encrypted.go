31ee86daace3aabb3f240b14e93f33546ee22c8d
cwx1077690 <caowei@huawei.com>
2021-10-23 15:59:53 +0800

    modify encrypted.go

--- encrypted.go
+++ encrypted.go
@@ -1,15 +1,16 @@
 package main
 
 import (
-"encoding/base64"
-"fmt"
+	"encoding/base64"
+	"fmt"
 )
 
 func main() {
 
-                 var password string
-	fmt.Println("please enter password£º")
+	var password string
+	fmt.Println("please enter password:")
+	fmt.Scanln(&password)
 	enc_password := base64.URLEncoding.EncodeToString([]byte(password))
-	fmt.Println("Encrypted password:"+enc_password)
+	fmt.Println("Encrypted password:" + enc_password)
 
 }
